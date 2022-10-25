5ce88d4462634113bb8a57cced0a3ae068fbd111
Fabricio Repetto <fabricio.j.repetto@gmail.com>
2022-10-25 18:32:06 -0300

    Update constants.js

--- client/chat-client/src/constants.js
+++ client/chat-client/src/constants.js
@@ -1,5 +1,5 @@
-const BACK_URL = 'https://accidental-rounded-cockatoo.glitch.me'
-// https://accidental-rounded-cockatoo.glitch.me
+const BACK_URL = 'https://websocket-project-server.glitch.me'
+// https://websocket-project-server.glitch.me
 // http://localhost:3000
 
 export { BACK_URL }
